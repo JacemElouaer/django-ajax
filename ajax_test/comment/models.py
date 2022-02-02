3b7943820c973b9606203cdf16a4e4be78d78e01
jasem-elwaar <jassem.elwaar@gmail.com>
2022-02-02 23:19:05 +0100

    fetch a get

--- ajax_test/comment/models.py
+++ ajax_test/comment/models.py
@@ -8,8 +8,7 @@ from django.contrib.auth.models import User
 class Author(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     fullname = models.CharField(max_length=255, blank=True, null=True)
-    image = models.ImageField()
-
+    image = models.ImageField(upload_to='static/image' ,  blank=True , null=True)
     def __str__(self):
         return self.fullname
 
@@ -20,7 +19,7 @@ class Comment(models.Model):
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return self.author
+        return self.author.fullname
 
     class Meta:
         ordering = ["-date"]
--- ajax_test/comment/views.py
+++ ajax_test/comment/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from django.http  import JsonResponse
+from django.http import JsonResponse
 from .models import *
+
+
 # Create your views here.
 
 
 def index(request):
-    return render(request,  'pages/index.html')
+    comments = Comment.objects.all()
+    context = {"comments": comments}
+    return render(request, 'pages/index.html', context)
+
 
 def getComments(request):
-    comments =  Comment.objects.all()
-    return  JsonResponse({"comments" :  list(comments)})
+    comments = Comment.objects.all()
+    return JsonResponse({"comments": list(comments)})
